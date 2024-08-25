7e30d8ae3b05926bac90c6c17ec6ea123c688055
wilmer mellizo <wmellizo93@gmial.com>
2024-08-25 17:00:13 -0500

    modificaciones menores

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,7 @@ fun main() {
     var op=Operaciones(2.0,3.0)
     op.suma()
     op.resta()
-println(op.div())
+    println(op.div())
     op.raiz(12.0)
     println(op.cuadrado(4.0))
 }
--- src/Operaciones.kt
+++ src/Operaciones.kt
@@ -6,18 +6,18 @@ class Operaciones(var n1:Double, var n2:Double){
         var suma = n1 + n2
         println("la suma es ${suma}")
     }
- fun resta (){
-     var resta = n1 -n2
-     println("la resta es ${resta}")
- }
- fun div ():Double{
-     return n1/n2
- }
-fun raiz (x:Double){
-    var raiz = Math.sqrt(x)
-    println(raiz)
-}
-fun cuadrado (c:Double):Double{
-    return Math.pow(c,2.0)
-}
+    fun resta (){
+        var resta = n1 -n2
+        println("la resta es ${resta}")
+    }
+    fun div ():Double{
+        return n1/n2
+    }
+    fun raiz (x:Double){
+        var raiz = Math.sqrt(x)
+        println(raiz)
+    }
+    fun cuadrado (c:Double):Double{
+        return Math.pow(c,2.0)
+    }
 }
